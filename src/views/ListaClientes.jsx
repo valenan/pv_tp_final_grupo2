460aa1f4f383ad99272a7ea2a9fc4ae1b01c5d29
valenan <valen.ang86@gmail.com>
2026-06-17 21:18:26 -0300

    Implementacion busqueda dinamica - Angulo, Chosco

--- src/views/ListaClientes.jsx
+++ src/views/ListaClientes.jsx
@@ -1,9 +1,10 @@
-import { use, useState } from 'react';
+import { useState } from 'react';
 import clientesService from '../services/clientesService';
 import ClienteCard from '../components/ClienteCard';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Container } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 
 function ListaClientes() {
     const [clientes, setClientes] = useState(
@@ -17,8 +18,15 @@ function ListaClientes() {
     };
     return (
         <div>
+            <h1>Pagina de listado de Clientes</h1>
             <div className='buscador'>
-
+            <Form>
+            <Form.Control
+            placeholder='Apellido o Lugar'
+            value={busqueda}
+            onChange={handleBusqueda}
+            ></Form.Control>
+            </Form>
             </div>
         {clientes.map(cliente => (
                 <ClienteCard
