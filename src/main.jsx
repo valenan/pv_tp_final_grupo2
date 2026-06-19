d62c225dc460f8930788d6b3a37bf4b7cf8aacce
valenan <valen.ang86@gmail.com>
2026-06-19 15:21:29 -0300

    Implementacion API - Angulo

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css' // se implemento para que el sistema grid funcione corectamente
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
